34011ff4edb50090845dcdc8ba4d3faf1e164533
Adalberto Pompolo <beto.nassu@gmail.com>
2024-11-09 13:05:44 -0300

    feat: details screen (FE only)

--- PhoneAgenda/ViewController.swift
+++ PhoneAgenda/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let mockList = ["Adalberto", "Nassu", "Pompolo"]
+let mockUserName = "Maria Vitoria Nassu Pompolo"
 
+class ViewController: UIViewController {
+    @IBOutlet weak var initialsLabel: UILabel!
+    @IBOutlet weak var initialsContainerView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialsContainerView.layer.borderWidth = 1
+        initialsContainerView.layer.borderColor = UIColor.white.cgColor
+        initialsContainerView.layer.cornerRadius = initialsContainerView.frame.height / 2
+        nameLabel.text = mockUserName
+        initialsLabel.text = getInitials(from: mockUserName)
     }
-
-
+    
+    private func getInitials(from name: String) -> String {
+        let names = name.split(separator: " ")
+        if let firstLetter = names.first?.first,
+            let secondLetter = names.last?.first {
+            return "\(firstLetter)\(secondLetter)"
+        }
+        return ""
+    }
 }
-
